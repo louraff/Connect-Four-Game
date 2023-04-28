8b4c64258110c3c687b1095af23e43e4c3449780
Louise Raffray <raffraylouise@gmail.com>
2023-04-28 13:59:44 +0100

    declared state variables

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@
 
 
 	/*----- state variables -----*/
+/* declaring state variables*/
+let board; /*array of 7 column arrays*/
+let turn; // 1 or -1 to represent player turns
+let winner; // holds vlues of null (no winner), 1 or -1 means there is winner. 'T' = tie game.
 
 
 	/*----- cached elements  -----*/
